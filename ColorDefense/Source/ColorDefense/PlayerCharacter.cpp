26f9eddb614543a177dc1b058c4e3cf371bbba84
s202255656 <s202255656@pusan.ac.kr>
2025-09-05 11:40:00 +0900

    [ 캐릭터 wasd, 점프 구현 ]

---
+++ ColorDefense/Source/ColorDefense/PlayerCharacter.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// 키 바인딩 매핑
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APlayerCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APlayerCharacter::LookRight);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &APlayerCharacter::Jump);
+}
+
+void APlayerCharacter::MoveForward(float AxisValue)
+{
+	// 무브먼트 컴포넌트(Movement Component)에 이동 요청을 추가
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+void APlayerCharacter::LookUp(float AxisValue)
+{
+	// 컨트롤러에 pitch 값 추가
+	AddControllerPitchInput(AxisValue);
+}
+
+void APlayerCharacter::MoveRight(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
+void APlayerCharacter::LookRight(float AxisValue)
+{
+	AddControllerYawInput(AxisValue);
+}
+
+void APlayerCharacter::Jump()
+{
+	ACharacter::Jump();
+}
---
+++ ColorDefense/Source/ColorDefense/PlayerCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class COLORDEFENSE_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	// 키 바인딩 wasd 마우스
+	void MoveForward(float AxisValue);
+	void LookUp(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookRight(float AxisValue);
+	void Jump();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
